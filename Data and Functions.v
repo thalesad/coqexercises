ff178be72d342919fae69e24e23644e7b692e405
thalesad <37780686+thalesad@users.noreply.github.com>
2018-07-05 22:49:02 -0300

    Exercises 1 and 2 in the same file (exercise 2 incomplete)
    Merged exercises 1 and 2 in the same .v file. Exercise 2 is still incomplete.

---
+++ Data and Functions.v
@@ -0,0 +1,35 @@
+
+(*Exercise: 1 star (nandb)*)
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | false => true
+  | true => negb(b2)
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+(*Exercise: 1 star (andb3)*)
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1 with
+  | false => false
+  | true => xxxxxxxx
+  end.
+
+Print andb3.
+
+Example test_andb31: (andb3 true true true) = true..
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
