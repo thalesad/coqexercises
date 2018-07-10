1d6b0c79d86271397b057aaee725268cc19e67be
thalesad <37780686+thalesad@users.noreply.github.com>
2018-07-10 17:42:12 -0300

    Exercise: 1 star (blt_nat)
    Added "Exercise: 1 star (blt_nat)". Not sure if it correct; Coq did not show errors though.

--- Data and Functions.v
+++ Data and Functions.v
@@ -171,3 +171,16 @@ Proof. simpl. reflexivity. Qed.
 Example test_leb3: (leb 4 2) = false.
 Proof. simpl. reflexivity. Qed.
 
+(*Exercise: 1 star (blt_nat)
+The blt_nat function tests natural numbers for less-than, yielding a boolean. Instead of making up a new Fixpoint for this one, define it in terms of a previously defined function.*)
+Definition blt_nat (n m : nat) : bool :=
+  match beq_nat n m with
+      | true => false
+      | false => leb n m
+  end.
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
