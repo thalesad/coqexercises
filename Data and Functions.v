6fc84c160e760ba6613243c5e1a2e999b5703394
thalesad <37780686+thalesad@users.noreply.github.com>
2018-07-10 14:59:58 -0300

    More examples
    "Fixpoints" examples. No new exercies yet.

--- Data and Functions.v
+++ Data and Functions.v
@@ -33,3 +33,84 @@ Proof. simpl. reflexivity. Qed.
 Example test_andb34: (andb3 true true false) = false.
 Proof. simpl. reflexivity. Qed.
 
+Check negb. (*show the function type*)
+
+(*Creates an alternative way to represent natural numbers*)
+Module NatPlayground.
+  Inductive nat : Type :=
+  | O : nat
+  | S : nat -> nat.
+
+  Inductive nat' : Type :=
+  | stop : nat'
+  | tick : nat' -> nat'.
+
+  Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End NatPlayground.
+
+Check (S (S (S (S O)))).
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') =>  n'
+  end.
+Compute (minustwo 4).
+Check S.
+Check pred.
+Check minustwo.
+
+(*Executes and print the result of a function*)
+Compute pred 2.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+Example test_oddb1: oddb 1 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: oddb 4 = false.
+Proof. simpl. reflexivity. Qed.
+
+Module NatPlayground2.
+  
+Fixpoint plus (n : nat) (m : nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Compute (plus 3 2).
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint minus (n m:nat) : nat :=
+  match n, m with
+  | O , _ => O
+  | S _ , O => n
+  | S n', S m' => minus n' m'
+  end.
+
+End NatPlayground2.
+
+Fixpoint exp (base power : nat) : nat :=
+  match power with
+    | O => S O
+    | S p => mult base (exp base p)
+  end.
