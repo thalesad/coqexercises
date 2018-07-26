8f5e048c526509a422b6005982adcc07204e8ca6
Thales <37780686+thalesad@users.noreply.github.com>
2018-07-26 14:49:03 -0300

    Ended "Proof by simplification" section
    Added more examples. There were no exercises on this section.

--- Data and Functions.v
+++ Data and Functions.v
@@ -211,3 +211,11 @@ Proof.
 Theorem plus_O_n' : forall n : nat, 0 + n = n.
 Proof.
   intros n. reflexivity. Qed.
+
+Theorem plus_1_l : forall n:nat, 1 + n = S n.
+Proof.
+  intros n. simpl. reflexivity. Qed. (*remove simpl. to go straighforward*)
+Theorem mult_0_l : forall n:nat, 0 * n = 0.
+Proof.
+  intros n. simpl. reflexivity. Qed. (*remove simpl. to go straighforward*)
+
