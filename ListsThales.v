16ef01e9356911b18d7379b09b5bb6c52985da7d
Thales <37780686+thalesad@users.noreply.github.com>
2018-09-19 17:18:31 -0300

    Added "Exercise: 2 stars, recommended (list_funs)"
    Added "Exercise: 2 stars, recommended (list_funs)"

--- ListsThales.v
+++ ListsThales.v
@@ -103,3 +103,87 @@ Fixpoint app (l1 l2 : natlist) : natlist :=
   | nil => l2
   | h :: t => h :: (app t l2)
   end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+Example test_app1: [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof.  reflexivity. Qed.
+Example test_app2: nil ++ [4;5] = [4;5].
+Proof. reflexivity. Qed.
+Example test_app3: [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity. Qed.
+
+Definition hd (default:nat) (l:natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+Definition tl (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+Example test_hd1: hd 0 [1;2;3] = 1.
+Proof. reflexivity. Qed.
+Example test_hd2: hd 0 [] = 0.
+Proof. reflexivity. Qed.
+Example test_tl: tl [1;2;3] = [2;3].
+Proof. reflexivity. Qed.
+
+(**Exercise: 2 stars, recommended (list_funs)
+Complete the definitions of nonzeros, oddmembers and countoddmembers below. Have a look at the tests to understand what these functions should do.*)
+Fixpoint nonzeros (l:natlist) : natlist  :=
+  match l with
+  | nil => nil
+  | h :: t => match h with
+              | O => nonzeros t
+              | S n => [h] ++ nonzeros t
+               end
+  end.
+              
+              
+Example test_nonzeros:
+  nonzeros [0;1;0;2;3;0;0] = [1;2;3].
+  simpl.
+  reflexivity.
+Qed.
+
+Compute oddb 4. (*Tests whether a number is odd*)
+
+Fixpoint oddmembers (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => match oddb h with
+             | true => [h] ++ oddmembers t
+             | false => oddmembers t
+             end
+  end.
+
+Example test_oddmembers:
+  oddmembers [0;1;0;2;3;0;0] = [1;3].
+  simpl.
+  reflexivity.
+Qed.
+
+Definition countoddmembers (l:natlist) : nat :=
+  match oddmembers l with
+  |nil => 0
+  |h :: t => 1 + length t
+  end.
+
+Compute countoddmembers [1;0;3;1;4;5;7;2;2;5].
+
+Example test_countoddmembers1:
+  countoddmembers [1;0;3;1;4;5] = 4.
+  reflexivity.
+Qed.
+
+Example test_countoddmembers2:
+  countoddmembers [0;2;4] = 0.
+reflexivity.
+  Qed.
+  
+Example test_countoddmembers3:
+  countoddmembers nil = 0.
+reflexivity.
+  Qed.
