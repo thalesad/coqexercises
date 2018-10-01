93cff32b9c221aa3ce09ccaa3bb25bb9e55d9967
Thales <37780686+thalesad@users.noreply.github.com>
2018-10-01 17:11:56 -0300

    Added examples about reasoning
    Added examples about reasoning about lists (all examples before the first list of exercises, which I'm about to start).

--- ListsThales.v
+++ ListsThales.v
@@ -394,3 +394,83 @@ Proof.
     simpl.
     reflexivity.
 Qed.
+
+Theorem nil_app : forall l:natlist,
+  [] ++ l = l.
+Proof. simpl. reflexivity. Qed.
+
+Theorem tl_length_pred : forall l:natlist,
+  pred (length l) = length (tl l).
+Proof.
+  intros l. destruct l as [| n l'].
+  - (* l = nil *)
+    simpl.
+    reflexivity.
+  - (* l = cons n l' *)
+    simpl.
+    reflexivity. Qed.
+
+Theorem app_assoc : forall l1 l2 l3 : natlist,
+  (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3).
+Proof.
+  intros l1 l2 l3. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    simpl.
+    reflexivity.
+  - (* l1 = cons n l1' *)
+    simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Fixpoint rev (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => rev t ++ [h]
+  end.
+Example test_rev1: rev [1;2;3] = [3;2;1].
+Proof. simpl. reflexivity. Qed.
+Example test_rev2: rev nil = nil.
+Proof. simpl. reflexivity. Qed.
+
+Theorem rev_length_firsttry : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l' IHl'].
+  - (* l =  *)
+    simpl.
+    reflexivity.
+  - (* l = n :: l' *)
+    (* This is the tricky case.  Let's begin as usual
+       by simplifying. *)
+    simpl.
+    (* Now we seem to be stuck: the goal is an equality
+       involving ++, but we don't have any useful equations
+       in either the immediate context or in the global
+       environment!  We can make a little progress by using
+       the IH to rewrite the goal... *)
+    rewrite <- IHl'.
+    (* ... but now we can't go any further. *)
+Abort.
+
+Theorem app_length : forall l1 l2 : natlist,
+  length (l1 ++ l2) = (length l1) + (length l2).
+Proof.
+  (* WORKED IN CLASS *)
+  intros l1 l2. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    simpl.
+    reflexivity.
+  - (* l1 = cons *)
+    simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Theorem rev_length : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l' IHl'].
+  - (* l = nil *)
+    reflexivity.
+  - (* l = cons *)
+    simpl. rewrite -> app_length, plus_comm.
+    simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Search rev.
+
+(**List Exercises, Part 1)
