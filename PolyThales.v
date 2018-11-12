23da0f51277bb6d8acdfb00bf48149436da66fc8
Thales <37780686+thalesad@users.noreply.github.com>
2018-11-12 15:26:40 -0200

    Added some polymorphism examples

--- PolyThales.v
+++ PolyThales.v
@@ -1,3 +1,28 @@
 Set Warnings "-notation-overridden,-parsing".
 Require Import ListsThales.
 
+Inductive list (X:Type) : Type :=
+  | nil : list X
+  | cons : X -> list X -> list X.
+
+Check list.
+Check (nil nat).
+Check (cons nat 3 (nil nat)).
+Check nil.
+Check cons.
+Check (cons nat 2 (cons nat 1 (nil nat))).
+
+Fixpoint repeat (X : Type) (x : X) (count : nat) : list X :=
+  match count with
+  | 0 => nil X
+  | S count' => cons X x (repeat X x count')
+  end.
+
+Example test_repeat1 :
+  repeat nat 4 2 = cons nat 4 (cons nat 4 (nil nat)).
+Proof. simpl. reflexivity. Qed.
+
+Example test_repeat2 :
+  repeat bool false 1 = cons bool false (nil bool).
+Proof. reflexivity. Qed.
+
